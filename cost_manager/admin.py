32144d9f15917624ee074cefb76b85fb8c942e64
Danil <abraham_lincoln5@rambler.ru>
2017-12-01 17:35:01 +0300

    start poj mood

--- cost_manager/admin.py
+++ cost_manager/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+
+admin.site.register(Post)
